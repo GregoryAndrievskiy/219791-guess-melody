872dc9a8e147014de887882ec5611510b8307391
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-06-16 00:07:25 +0400

    `fixMusicURL`

--- js/gameData.js
+++ js/gameData.js
@@ -1,5 +1,5 @@
 export default {
-  genre: [`Хип-хоп`, `Классические`, `Кантри`, `Поп`],
-  artist: [`Twin Musicom`, `Mozart`, `Unicorn Heads`, `Silent Partner`],
-  url: [`https://www.youtube.com/audiolibrary_download?vid=1d01174c384ac88c`, `https://www.youtube.com/audiolibrary_download?vid=5f37e1a59d003588`, `https://www.youtube.com/audiolibrary_download?vid=4bae71dd4663c88d`, `https://www.youtube.com/audiolibrary_download?vid=47826af5576d3ae4`]
+  genre: [`Хип-хоп`, `Классические`, `Кантри`, `Поп`, `Рок`, `Джаз`],
+  artist: [`Twin Musicom`, `Mozart`, `Unicorn Heads`, `Silent Partner`, `	Ethan Meixsell`, `Aaron Lieberman`],
+  url: [`https://www.youtube.com/audiolibrary_download?vid=1d01174c384ac88c`, `https://www.youtube.com/audiolibrary_download?vid=5f37e1a59d003588`, `https://www.youtube.com/audiolibrary_download?vid=f809826f4780792d`, `https://www.youtube.com/audiolibrary_download?vid=47826af5576d3ae4`, `https://www.youtube.com/audiolibrary_download?vid=21f29f75302b20ad`, `https://www.youtube.com/audiolibrary_download?vid=9da06527af146444`]
 };
